84b6267154d93ced006a10e26b93f98c6791057f
Shyam Grover <post2shyam@gmail.com>
2022-05-19 19:30:05 +0530

    Enabled viewbinding.

--- app/src/main/java/com/android/screenrecorder/MainActivity.kt
+++ app/src/main/java/com/android/screenrecorder/MainActivity.kt
@@ -1,11 +1,17 @@
 package com.android.screenrecorder
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.android.screenrecorder.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
