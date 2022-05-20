7fc355237a61860941f98896dde9c3682d2006ec
Shyam Grover <post2shyam@gmail.com>
2022-05-20 10:30:57 +0530

    Start / Stop logging functionality now tied to Play/Stop button.

--- app/src/main/java/com/android/screenrecorder/screens/MainActivity.kt
+++ app/src/main/java/com/android/screenrecorder/screens/MainActivity.kt
@@ -3,10 +3,15 @@ package com.android.screenrecorder.screens
 import android.os.Bundle
 import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.coroutineScope
 import com.android.screenrecorder.R
 import com.android.screenrecorder.databinding.ActivityMainBinding
 import com.android.screenrecorder.system.logger.RollingFileTimberTree
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.isActive
+import kotlinx.coroutines.launch
 import timber.log.Timber
 import javax.inject.Inject
 
@@ -25,22 +30,34 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        setupUi()
         rollingFileTimberTree.start()
-        Timber.tag("SSG").d("onCreate:")
-        rollingFileTimberTree.stop()
-        Timber.tag("SSG2").d("onCreate:")
+
+        setupUi()
+        startDummyLogging()
+    }
+
+    private fun startDummyLogging() {
+        lifecycle.coroutineScope.launch(Dispatchers.IO) {
+            var i = 0
+            while (isActive) {
+                delay(2000)
+                Timber.tag("SSG").d("${i++}")
+            }
+        }
     }
 
     private fun setupUi() {
         with(binding) {
             playStopButton.setOnClickListener {
+
                 val button = it as Button
 
                 //Toggle current text of the button
                 button.text = if (button.text.equals(getString(R.string.play))) {
+                    rollingFileTimberTree.stop()
                     getString(R.string.stop)
                 } else {
+                    rollingFileTimberTree.start()
                     getString(R.string.play)
                 }
             }
